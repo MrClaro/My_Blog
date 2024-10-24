9e89ef4015eb74a13aff771dfd2cd5c64347acd8
MrClaro <adryan.contatoprofissional@gmail.com>
2024-10-24 18:28:18 -0300

    Refactor(Header): Refactored the header of the client side to interpret the user context

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,18 +1,51 @@
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 export default function Heaer() {
+  const { setUserInfo, userInfo } = useContext(UserContext);
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo);
+      });
+    });
+  }, []);
+
+  function logout() {
+    fetch("http://localhost:4000/logout", {
+      credentials: "include",
+      method: "POST",
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
   return (
     <header>
       <Link to="/" className="logo">
         MyBlog
       </Link>
       <nav>
-        <Link to="/Login" className="login">
-          Login
-        </Link>
-        <Link to="/Register" className="Register">
-          Register
-        </Link>
+        {username && (
+          <>
+            <Link to={"/create"}>Create new post</Link>
+            <Link onClick={logout}>Logout</Link>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login" className="login">
+              Login
+            </Link>
+            <Link to="/register" className="Register">
+              Register
+            </Link>
+          </>
+        )}
       </nav>
     </header>
   );
